0d4cc03a0d7e70fef137d6f19292af158be6f8db
Pete Seeling <peteseeling@gmail.com>
2022-01-31 16:43:21 -0600

    waterfront dropdown finished

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   // track some state here.
   const [setCastleId, castleId] = useState(1);
   const [setSkylineId, skylineId] = useState(1);
+  const [setWaterfrontId, waterFrontId] = useState(1);
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
   // you'll need to track a city name, which starts as the city name of your choice.
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
@@ -19,7 +20,7 @@ function App() {
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
-      <City castleId={castleId} skylineId={skylineId}/>
+      <City castleId={castleId} skylineId={skylineId} waterFrontId={waterFrontId}/>
       <h1>
         {/* dynamically update the city name here using state */}
         Welcome to beautiful Portland!
@@ -35,6 +36,7 @@ function App() {
           <div>
             <CastleDropdown setCastleId={setCastleId} />
             <SkylineDropdown setSkylineId={setSkylineId} />
+            <WaterfrontDropdown setWaterfrontId={setWaterfrontId} />
           
           </div>
         </section>
